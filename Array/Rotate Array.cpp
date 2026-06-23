4258b9943fab5a11ea0c848582e017199576da35
nishapanchal07 <niishapanchal2006@gmail.com>
2026-06-23 21:36:19 +0530

    Move Zeroes

--- Array/Rotate Array.cpp
+++ Array/Rotate Array.cpp
@@ -1,2 +1,12 @@
 /* 189 :  Given an integer array nums, rotate the array to the right by k steps, where k is non-negative. */
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
 
+        reverse(nums.begin(), nums.end());          // whole array
+        reverse(nums.begin(), nums.begin() + k);    // first k elements
+        reverse(nums.begin() + k, nums.end());      // remaining elements
+    }
+};
