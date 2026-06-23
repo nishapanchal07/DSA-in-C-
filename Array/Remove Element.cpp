fbbac3ae4e3b8c51f2bc87b9654b624f00d04d4c
nishapanchal07 <niishapanchal2006@gmail.com>
2026-06-23 21:34:44 +0530

    Rotate Array

--- Array/Remove Element.cpp
+++ Array/Remove Element.cpp
@@ -5,3 +5,17 @@ Consider the number of elements in nums which are not equal to val be k, to get 
 Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 Return k. */
 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0 ;
+        for(int j = 0 ; j<nums.size() ; j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
---
+++ Array/Rotate Array.cpp
@@ -0,0 +1,2 @@
+/* 189 :  Given an integer array nums, rotate the array to the right by k steps, where k is non-negative. */
+
