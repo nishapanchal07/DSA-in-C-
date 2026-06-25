8ef3de9be6f9f687b35b0a9a42c4bcf27f0fddc4
nishapanchal07 <niishapanchal2006@gmail.com>
2026-06-25 17:08:40 +0530

    intersection of two arrays

---
+++ Array/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,35 @@
+/*Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.*/
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0;
+        int j = 0;
+
+        while (i < n && j < m) {
+            if (nums1[i] == nums2[j]) {
+                if (ans.empty() || ans.back() != nums1[i])
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return ans;
+    }
+}; 
