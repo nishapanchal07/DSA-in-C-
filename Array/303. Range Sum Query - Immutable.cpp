e25151782ea2310b7d1107662d645b4eed2d79a3
nishapanchal07 <niishapanchal2006@gmail.com>
2026-06-26 12:55:31 +0530

    range sum query

--- Array/303. Range Sum Query - Immutable.cpp
+++ Array/303. Range Sum Query - Immutable.cpp
@@ -5,7 +5,29 @@ Implement the NumArray class:
 
 NumArray(int[] nums) Initializes the object with the integer array nums.
 int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
- 
-
 */
 
+class NumArray {
+public:
+    vector<int> prefix;
+
+    NumArray(vector<int>& nums) {
+
+        prefix.push_back(nums[0]);
+
+        for(int i=1;i<nums.size();i++){
+            prefix.push_back(
+                prefix[i-1]+nums[i]
+            );
+        }
+    }
+
+    int sumRange(int left,int right){
+
+        if(left==0)
+            return prefix[right];
+
+        return prefix[right]
+              -prefix[left-1];
+    }
+};
